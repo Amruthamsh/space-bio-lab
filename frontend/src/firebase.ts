f056acb1c7f62b27911278dfd3be6abfe04bc418
Amruthamsh A <amruthaamsh@gmail.com>
2024-12-20 15:36:32 +0530

    config

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCPBNm_xqoqVc3hY0m8_z1MCnDVoiEZMfg",
-  authDomain: "space-bio-lab.firebaseapp.com",
-  projectId: "space-bio-lab",
-  storageBucket: "space-bio-lab.appspot.com",
-  messagingSenderId: "304227685831",
-  appId: "1:304227685831:web:fc29f1d294276d2fb475e2",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
